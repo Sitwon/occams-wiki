54d7b54c858e725db629d988d1b6c8b9d83b471d
Ben Mendis <dragonwisard@gmail.com>
2010-04-23 12:35:06 -0400

    Ok, fixed. Now I can rename occam.py whatever I like.

--- output/render/CamelCase.sh
+++ output/render/CamelCase.sh
@@ -3,6 +3,6 @@
 while read line
 do
 	echo $line|
-	sed 's/\<[A-Z][a-z]\+[A-Z][a-z][A-Za-z]*\>/<a href="occam.py?\0">\0<\/a>/g'
+	sed "s/\<[A-Z][a-z]\+[A-Z][a-z][A-Za-z]*\>/<a href=\"$(echo ${SCRIPT_NAME} | sed 's/\//\\\//g')?\0\">\0<\\/a>/g"
 done
 
